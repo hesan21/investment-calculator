7c3316003c97f3305c7f0ef94f636572c048c3d8
hesan21 <hesannaveed21@gmail.com>
2024-11-06 17:30:36 +0500

    React Configs Fix

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'build',  // Set output directory to 'build'
-  }
+    base: '/investment-calculator',
+    plugins: [react()],
+    build: {
+        outDir: 'build',  // Set output directory to 'build'
+        assetsDir: 'assets'
+    }
 })
